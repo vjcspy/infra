efe6f76fc4743059164ea725041ca70c9cf29477
ubuntu-desktop <mr.vjcspy@gmail.com>
2025-01-12 18:23:53 +0700

    feat: separate splunk and license server to 2 pods

--- misc/docker/splunk-server/start.sh
+++ misc/docker/splunk-server/start.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-# Chạy dvt-splunk_licsrv với port 3001
-/app/dvt-splunk_licsrv.1.0.linux.amd64 -port 3001 &
+# Trap SIGTERM and SIGINT
+trap 'cleanup' TERM INT
 
+cleanup() {
+    echo "Stopping processes..."
+    [ -n "$LICENSE_SERVER_PID" ] && kill $LICENSE_SERVER_PID
+    cd /app/splunk/bin && ./splunk stop
+    exit 0
+}
+
+# Start Splunk
 (
     chmod +x /app/splunk/bin/splunk &&
     cd /app/splunk/bin || exit 1
